2841e04dc35d436cfc665765a9d6762d23fddee2
zdietzen <diezel37@gmail.com>
2015-10-06 12:04:36 -0700

    first push before lunch
    Getting the back end finished up.

---
+++ WeatherApp/WeatherApp/Classes/CurrentConditions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Classes
+{
+    public class CurrentConditions
+    {
+        public string elevation { get; set; }
+        public string lon { get; set; }
+        public string lat { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string temperature_string { get; set; }
+        public string feelslike_string { get; set; }
+        public string wind_string { get; set; }
+        public string wind_dir { get; set; }
+        public string relative_humidity { get; set; }
+        public string visibility_mi { get; set; }
+        public string precip_today_string { get; set; }
+        public string weather { get; set; }
+    }
+}
--- WeatherApp/WeatherApp/MainWindow.xaml.cs
+++ WeatherApp/WeatherApp/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WeatherApp.Classes;
 
 namespace WeatherApp
 {
@@ -27,7 +30,44 @@ namespace WeatherApp
 
         private void SearchButton_Click(object sender, RoutedEventArgs e)
         {
-            string CaptureZip = SearchBar.Text;
+            string zip = SearchBar.Text;
+
+
+            using (WebClient webClient = new WebClient())
+                
+
+            {
+                //Tells the Weather Underground API who's boss
+                webClient.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+
+                // Grabs json info from github and saves it to a string.
+                string json = webClient.DownloadString("http://api.wunderground.com/api/a3bf043be8c7fc4a/geolookup/conditions/q/" + zip + ".json");
+
+                //this converts the json info into usable information for us
+                currentConditions = JsonConvert.DeserializeObject<CurrentConditions>(json);
+
+                //This stores the information that was converted to strings
+                string Weather = currentConditions.weather;
+                string Elevation = currentConditions.elevation;
+                string Longitude = currentConditions.lon;
+                string Latitude = currentConditions.lat;
+                string LocationState = currentConditions.state;
+                string LocationCity = currentConditions.city;
+                string Temperature = currentConditions.temperature_string;
+                string FeelsLike = currentConditions.feelslike_string;
+                string Wind = currentConditions.wind_string;
+                string WindDirection = currentConditions.wind_dir;
+                string Humidity = currentConditions.relative_humidity;
+                string UV = currentConditions.UV;
+                string Visibility = currentConditions.visibility_mi;
+                string Percipitation = currentConditions.precip_today_string;
+
+                // writes the information out to the console
+
+            }
         }
     }
+
 }
+    
+
